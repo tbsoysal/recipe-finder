baa528c530fbecda1c738cd1d883a276a388f5f3
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-09-12 17:05:43 +0300

    add expression for no results

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const dataListEl = document.querySelector('.data-list'); // ul
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const reloading = `<h2 style="text-align:center;">Loading...</h2>`;
+  const reloading = `<h2 style="text-align:center;">Searching...</h2>`;
   dataListEl.innerHTML = reloading;
   const search_keyword = input.value;
   ListRecipes(search_keyword);
@@ -22,6 +22,11 @@ async function ListRecipes(keyword) {
 }
 
 function addRecipesToDOM(recipes) {
+  if (recipes.length < 1) {
+    dataListEl.innerHTML = "No result! Please search for a diffrent keyword.";
+    return;
+  }
+
   dataListEl.innerHTML = "";
 
   recipes.map((recipeObj) => {
@@ -31,7 +36,7 @@ function addRecipesToDOM(recipes) {
         <div class="content-container">
           <h2><a href=${recipeObj.recipe.url}>${recipeObj.recipe.label}</a></h2>
           <ul>
-            ${recipeObj.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`)}
+            ${recipeObj.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
           </ul>
         </div>
       </li>
